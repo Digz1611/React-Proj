c0fe99ea2467e3dc0babd3933c71e6ae6ead02c8
Digz1611 <diego.langeveldt@gmail.com>
2024-09-04 10:03:58 +0200

    sorted something  quick

--- ExpeditionEase/src/components/AddItinerary.js
+++ ExpeditionEase/src/components/AddItinerary.js
@@ -1,8 +1,7 @@
 // src/components/AddItinerary.js
 import React, { useState } from 'react';
 import { collection, addDoc } from 'firebase/firestore';
-import { db, auth, storage } from '../firebaseConfig';
-import { ref, uploadBytesResumable, getDownloadURL } from 'firebase/storage';
+import { db, auth } from '../firebaseConfig';
 import './styles/AddItinerary.module.css';
 
 const AddItinerary = () => {
@@ -12,41 +11,12 @@ const AddItinerary = () => {
   const [accommodation, setAccommodation] = useState('');
   const [transportation, setTransportation] = useState('');
   const [cost, setCost] = useState('');
-  const [itineraryPhoto, setItineraryPhoto] = useState(null);
-  const [uploadProgress, setUploadProgress] = useState(0);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
       const user = auth.currentUser;
       if (user) {
-        let photoURL = null; // Initialize photoURL to null
-
-        if (itineraryPhoto) {
-          const storageRef = ref(storage, `itinerary-photos/${itineraryPhoto.name}`);
-          const uploadTask = uploadBytesResumable(storageRef, itineraryPhoto);
-
-          await new Promise((resolve, reject) => {
-            uploadTask.on(
-              'state_changed',
-              (snapshot) => {
-                const progress = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
-                setUploadProgress(progress);
-              },
-              (error) => {
-                console.error('Error uploading photo:', error);
-                reject(error);
-              },
-              () => {
-                getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                  photoURL = downloadURL; // Update photoURL with the download URL
-                  resolve();
-                }).catch(reject);
-              }
-            );
-          });
-        }
-
         await addDoc(collection(db, 'itinerarys'), {
           destination,
           date,
@@ -56,17 +26,15 @@ const AddItinerary = () => {
           cost,
           userId: user.uid,
           userEmail: user.email,
-          photoURL, // Use the updated photoURL value
         });
 
+        // Reset form fields
         setDestination('');
         setDate('');
         setAttractions('');
         setAccommodation('');
         setTransportation('');
         setCost('');
-        setItineraryPhoto(null);
-        setUploadProgress(0);
       } else {
         console.error("User not authenticated");
       }
@@ -76,82 +44,73 @@ const AddItinerary = () => {
   };
 
   return (
-<div>
-  <h2>Add Itinerary:</h2>
-  <form onSubmit={handleSubmit}>
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Destination:</h3>
-      <input
-        type="text"
-        value={destination}
-        onChange={(e) => setDestination(e.target.value)}
-        placeholder="Give the Destination however you so please..."
-        required
-      />
-    </div>
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Date:</h3>
-      <input
-        type="text"
-        value={date}
-        onChange={(e) => setDate(e.target.value)}
-        placeholder="Give the Date however you so please..."
-        required
-      />
-    </div>
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Attractions:</h3>
-      <textarea
-        value={attractions}
-        onChange={(e) => setAttractions(e.target.value)}
-        placeholder="Detail the places you plan to visit and what you want to experience or see..."
-        required
-      />
-    </div>
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Accommodation:</h3>
-      <textarea
-        value={accommodation}
-        onChange={(e) => setAccommodation(e.target.value)}
-        placeholder="Provide a list of places you'd like to stay, along with links to them..."
-        required
-      />
-    </div>
+    <div>
+      <h2>Add Itinerary:</h2>
+      <form onSubmit={handleSubmit}>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Destination:</h3>
+          <input
+            type="text"
+            value={destination}
+            onChange={(e) => setDestination(e.target.value)}
+            placeholder="Give the Destination however you so please..."
+            required
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Date:</h3>
+          <input
+            type="text"
+            value={date}
+            onChange={(e) => setDate(e.target.value)}
+            placeholder="Give the Date however you so please..."
+            required
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Attractions:</h3>
+          <textarea
+            value={attractions}
+            onChange={(e) => setAttractions(e.target.value)}
+            placeholder="Detail the places you plan to visit and what you want to experience or see..."
+            required
+          />
+        </div>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Accommodation:</h3>
+          <textarea
+            value={accommodation}
+            onChange={(e) => setAccommodation(e.target.value)}
+            placeholder="Provide a list of places you'd like to stay, along with links to them..."
+            required
+          />
+        </div>
 
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Transportation:</h3>
-      <input
-        type="text"
-        value={transportation}
-        onChange={(e) => setTransportation(e.target.value)}
-        placeholder="Give all means of Transportation that would be used..."
-        required
-      />
-    </div>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Transportation:</h3>
+          <input
+            type="text"
+            value={transportation}
+            onChange={(e) => setTransportation(e.target.value)}
+            placeholder="Give all means of Transportation that would be used..."
+            required
+          />
+        </div>
 
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Cost:</h3>
-      <input
-        type="text"
-        value={cost}
-        onChange={(e) => setCost(e.target.value)}
-        placeholder="Give the est Cost to make the trip happen..."
-        required
-      />
-    </div>
+        <div style={{ marginBottom: '20px' }}>
+          <h3>Cost:</h3>
+          <input
+            type="text"
+            value={cost}
+            onChange={(e) => setCost(e.target.value)}
+            placeholder="Give the est Cost to make the trip happen..."
+            required
+          />
+        </div>
 
-    <div style={{ marginBottom: '20px' }}>
-      <h3>Itinerary Photo:</h3>
-      <h5>OPTIONAL: Image relating the trip...</h5>
-      <input
-        type="file"
-        onChange={(e) => setItineraryPhoto(e.target.files[0])}
-      />
-      {uploadProgress > 0 && <progress value={uploadProgress} max="100" />}
+        <button type="submit">Add Itinerary</button>
+      </form>
     </div>
-    <button type="submit">Add Itinerary</button>
-  </form>
-</div>
   );
 };
 
